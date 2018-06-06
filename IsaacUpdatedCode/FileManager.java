03371e25e7c6095517e27cdec5bb5f18f0757580
ADMIRALxISAAC <39735850+ADMIRALxISAAC@users.noreply.github.com>
2018-06-06 10:53:19 -0400

    Update FileManager.java

--- IsaacUpdatedCode/FileManager.java
+++ IsaacUpdatedCode/FileManager.java
@@ -30,7 +30,7 @@ import javax.swing.JPanel;
 public class FileManager {
 	static int w = Toolkit.getDefaultToolkit().getScreenSize().width;//width of screen
 	static int l = Toolkit.getDefaultToolkit().getScreenSize().height;//height of screen
-	private static JButton btnLS1  = new JButton();
+	private static JButton btnLS1  = new JButton();//each of these buttons will be used to access the 8 different save files
 	private static JButton btnLS2  = new JButton();
 	private static JButton btnLS3  = new JButton();
 	private static JButton btnLS4  = new JButton();
@@ -38,15 +38,21 @@ public class FileManager {
 	private static JButton btnLS6  = new JButton();
 	private static JButton btnLS7  = new JButton();
 	private static JButton btnLS8  = new JButton();
-	private static final String[] files={"FencingSym_1.txt","FencingSym_2.txt","FencingSym_3.txt","FencingSym_4.txt","FencingSym_5.txt","FencingSym_6.txt","FencingSym_7.txt","FencingSym_8.txt"};
+	private static final String[] files={"FencingSym_1.txt","FencingSym_2.txt","FencingSym_3.txt","FencingSym_4.txt","FencingSym_5.txt","FencingSym_6.txt","FencingSym_7.txt","FencingSym_8.txt"};//this holds the file names for each save file IMPORTANT
 	public static JFrame main=new JFrame("Fencing Simulator 2K18");//container for the menu
 	private static JLabel back=new JLabel();//will be used for backround
-	private static JLabel lblText=new JLabel();//will be used for backround
+	private static JLabel lblText=new JLabel();//propts user to make a selection
 	private static JPanel panel=new JPanel();//panel to hold buttons
 	
 	
 	
-	
+	//name: loadSave
+	//parameters: String
+	//Returns: GameSave
+	//Dependencies: java.util.*; java.IO.*;
+	//Last Modified: June 6, 2018
+	//throws: none
+	//Description: takes in information from file to set up a previously started game from where it left off
 	private static GameSave loadSave(String flname){
 		GameSave nm=new GameSave();
 		FileInputStream FileIn; // create a file input object
@@ -85,7 +91,15 @@ public class FileManager {
 		return nm;
 		
 	}
+	//name: initialize
+	//parameters: none
+	//Returns: none
+	//Dependencies: back, main
+	//Last Modified: June 6th 2018
+	//throws: none
+	//Description: sets up all the buttons, label, and background
 	private static void initialize(){
+		//creates the backround image for the frame
 		back.setLayout(new BorderLayout());
 		try{
 	    	back.setIcon(new ImageIcon(ImageIO.read(new File("backFire.png"))));
@@ -96,7 +110,7 @@ public class FileManager {
 		
 		//panel.setLayout(new BorderLayout());
 		
-		btnLS1.setBounds(0, 0, 50, 30);
+		btnLS1.setBounds(0, 0, 50, 30);//sets the sizes of the buttons
 		btnLS2.setBounds(0, 0, 50, 30);
 		btnLS3.setBounds(0, 0, 50, 30);
 		btnLS4.setBounds(0, 0, 50, 30);
@@ -105,15 +119,16 @@ public class FileManager {
 		btnLS7.setBounds(0, 0, 50, 30);
 		btnLS8.setBounds(0, 0, 50, 30);
 		
-		lblText.setBounds(0, 0, 100, 30);
+		lblText.setBounds(0, 0, 100, 30);//sets the lable dimensions
 		
-		Font f=new Font("Times New Roman", 1, 25);
+		//sets up label
+		Font f=new Font("Impact", 0, 25);
 		lblText.setBackground(null);
 		lblText.setForeground(Color.WHITE);
 		lblText.setFont(f);
 		lblText.setText("Choose save file to load.");
 		
-		btnLS1.setText(files[0]);
+		btnLS1.setText(files[0]);//renames each button to their corrisponding file names
 		btnLS2.setText(files[1]);
 		btnLS3.setText(files[2]);
 		btnLS4.setText(files[3]);
@@ -123,7 +138,7 @@ public class FileManager {
 		btnLS8.setText(files[7]);
 		
 		
-		btnLS1.setBackground(Color.RED);
+		btnLS1.setBackground(Color.RED);//sets button backrounds
 		btnLS2.setBackground(Color.RED);
 		btnLS3.setBackground(Color.RED);
 		btnLS4.setBackground(Color.RED);
@@ -132,7 +147,7 @@ public class FileManager {
 		btnLS7.setBackground(Color.RED);
 		btnLS8.setBackground(Color.RED);
 		
-		btnLS1.setForeground(Color.BLACK);
+		btnLS1.setForeground(Color.BLACK);//sets button foregrounds
 		btnLS2.setForeground(Color.BLACK);
 		btnLS3.setForeground(Color.BLACK);
 		btnLS4.setForeground(Color.BLACK);
@@ -141,9 +156,10 @@ public class FileManager {
 		btnLS7.setForeground(Color.BLACK);
 		btnLS8.setForeground(Color.BLACK);
 		
+		//makes panel background invisible
 		panel.setOpaque(false);
 		
-		panel.add(btnLS1, BorderLayout.CENTER);
+		panel.add(btnLS1, BorderLayout.CENTER);//add buttons to panel
 		panel.add(btnLS2, BorderLayout.CENTER);
 		panel.add(btnLS3, BorderLayout.CENTER);
 		panel.add(btnLS4, BorderLayout.CENTER);
@@ -152,11 +168,11 @@ public class FileManager {
 		panel.add(btnLS7, BorderLayout.CENTER);
 		panel.add(btnLS8, BorderLayout.CENTER);
 		
-		main.add(lblText, BorderLayout.SOUTH);
-		main.add(panel, BorderLayout.CENTER);
-		main.setBackground(null);
+		main.add(lblText, BorderLayout.SOUTH);//adds lablel to frame
+		main.add(panel, BorderLayout.CENTER);//adds panel to frame
+		main.setBackground(null);//makes default frame background invisible
 		
-		btnLS1.setVisible(true);
+		btnLS1.setVisible(true);//makes everything visible
 		btnLS2.setVisible(true);
 		btnLS3.setVisible(true);
 		btnLS4.setVisible(true);
@@ -168,10 +184,17 @@ public class FileManager {
 		lblText.setVisible(true);
 	    
 	    
-	    main.setResizable(false);
+	    main.setResizable(false);//user can no longer manipulate the size of the frame
 	    main.pack();
 	    main.setVisible(true);	    
-	}	
+	}
+	//name: start
+	//parameters: none
+	//Returns: none
+	//Dependencies: w, l, main, java.awt.*;
+	//Last Modified: June 6th 2018
+	//throws: none
+	//Description: sets up and starts entire GUI
 	private static void start(){
 		main.setSize(w, l);
 		main.setLocation(new Point(0,0));
@@ -184,7 +207,13 @@ public class FileManager {
 	    initialize();
 		initEvent();	   
 	}
-	
+	//name: initEvent
+	//parameters: none
+	//Returns: none
+	//Dependencies: main, loadsave, java.awt.*;
+	//Last Modified: June 6th 2018
+	//throws: none
+	//Description: checks for and reacts to button clicks and exiting
 	private static void initEvent(){
 
 	    main.addWindowListener(new WindowAdapter() {
@@ -235,6 +264,13 @@ public class FileManager {
 		});
 	    
 	}
+	//name: FileManager
+	//parameters: none
+	//Returns: none
+	//Dependencies: start
+	//Last Modified: june 6th 2018
+	//throws: none
+	//Description: constructor for object
 	FileManager(){
 		start();
 	}
@@ -244,3 +280,4 @@ public class FileManager {
 	
 		
 }
+
