38ca42c236184dfc559a7da189ff34f82884e474
ADMIRALxISAAC <39735850+ADMIRALxISAAC@users.noreply.github.com>
2018-06-14 10:12:31 -0400

    Update Fencer.java

--- src/character/Fencer.java
+++ src/character/Fencer.java
@@ -2,13 +2,17 @@
 //Date Created: June. 1, 2018
 //Last modified: June. 14, 2018
 //Fencing Simulator 2018
-//program: Dr. Evil and Batman with Richard Dean Anderson star in: Fencing Symulator 2K18: Stabby Mc Kill Die Too: Electric Boogaloo: The Phantom Menace: Attack of the Clones: Revenge of the Sith: Wrath of Khan Part 2: Dead Man’s Chest: The third one, part 7 of 9 in the trilogy: Prequel to the Quran, by Sun Tzu and Robert Munch With Samuel L Jackson as “God” Based on a true story as told by Tommy Wiseau
+//program: Dr. Evil and Batman with Richard Dean Anderson star in: Fencing Symulator 2K18: Stabby Mc Kill Die Too: Electric Boogaloo: The Phantom Menace: Attack of the Clones: Revenge of the Sith: Wrath of Khan Part 2: Dead Manâ€™s Chest: The third one, part 7 of 9 in the trilogy: Prequel to the Quran, by Sun Tzu and Robert Munch With Samuel L Jackson as â€œGodâ€ Based on a true story as told by Tommy Wiseau
 package character;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import Ai.AI;
+/**
+* the fencer object
+* @author Brad
+**/
 public class Fencer extends Rectangle.Double{
 	/*ADD DEFAULT VALUES JUST IN CASE
 	 * 
@@ -22,20 +26,32 @@ private int jump=0;
 private int score=0;
 private double ground;
 /**
- * @return the score
- */
+* @author Brad
+* @return the score
+**/
 public int getScore() {
 	return score;
 }
 /**
- * @param score the score to set
- */
+* @author Brad
+* @param score the score to set
+**/
 public void setScore(int score) {
 	this.score = score;
 }
 
 private BufferedImage[] FncP=new BufferedImage[5] ;
+	
+/**
+* Constructor for class
+* @author Brad
+**/
 public Fencer(){}
+/**
+* Constructor for class
+* @author Brad
+* @param BufferedImage[] FncP,String Name,int Speed,double x,double y,int height,int width,String NameB,int speedB,double length, Point.Double handle,Point.Double tip, int block, int lunge
+**/
 public Fencer(BufferedImage[] FncP,String Name,int Speed,double x,double y,int height,int width,String NameB,int speedB,double length, Point.Double handle,Point.Double tip, int block, int lunge){
 	this.Name=Name;
 	this.Speed=Speed;
@@ -47,11 +63,21 @@ public Fencer(BufferedImage[] FncP,String Name,int Speed,double x,double y,int h
 	this.Sword=new Blade(NameB,speedB,length, handle, tip, block, lunge);
 	this.FncP= FncP;
 }
+/**
+* @author Brad
+* @param Point.Double
+* @return boolean
+**/	
 public boolean hit(Point.Double p){
 	if(this.contains(p)){
 		return true;
 	}return false;
 }
+/**
+* moves the fencer
+* @author Brad
+* @param boolean
+**/
 public void move(boolean forwards){
 	if(forwards){
 		x+=Speed;
@@ -67,6 +93,11 @@ public void move(boolean forwards){
  * 6/4/18
  * changes y coordinate to allow character to jump
  */
+/**
+*jumping function
+* @author Brad
+* @return boolean
+**/
 public boolean jump(){
 	boolean yay=true;
 	if(y>Th*(0.60185185)-120&&(jump==0||jump==1)){
@@ -89,6 +120,11 @@ public boolean jump(){
  * 6/4/18
  * casues fencer to lunge towords enemy and checks if blocked 
  */
+/**
+*lunging function
+* @author Brad
+* @param Fencer
+**/
 public void lunge(Fencer enemy){
 	if(!(Sword.lungeCD>0)){
 		if(!Sword.colisionBlade(Sword, enemy.getSword())){
@@ -101,6 +137,12 @@ public void lunge(Fencer enemy){
 //FencerControl
 //6/4/18
 //returns 1=win,-1=block,0=nothing
+/**
+*controls the fencer 
+* @author Brad
+* @param double, double, fencer
+* @return int
+**/
 public int FencerControl(double x,double y,Fencer enemy){
 	int rtrn=0;
 	
@@ -112,33 +154,71 @@ public int FencerControl(double x,double y,Fencer enemy){
 		rtrn=-1;
 	}return rtrn;
 }
+/**
+*checks that the blade is moving
+* @author Brad
+* @param double, double, fencer
+* @return boolean
+**/
 public boolean moveBlade(double x,double y,Fencer enemy){
 	Sword.bladeMove(x,y);
 	return enemy.hit(Sword.tip);
 }
 //getters and setters
+/** 
+* @author Brad
+* @return int
+**/
 public int getSpeed() {
 	return Speed;
 }
+/** 
+* @author Brad
+* @param int
+* @return BufferedImage
+**/
 public BufferedImage getPic(int i) {
 	return FncP[i];
 }
+/** 
+* @author Brad
+* @param int
+**/
 public void setSpeed(int speed) {
 	Speed = speed;
 }
+/** 
+* @author Brad
+* @return String
+**/
 public String getName() {
 	return Name;
 }
+/** 
+* @author Brad
+* @param String
+**/
 public void setName(String name) {
 	Name = name;
 }
+/** 
+* @author Brad
+* @return Blade
+**/
 public Blade getSword() {
 	return Sword;
 }
+/** 
+* @author Brad
+* @param Blade
+**/
 public void setSword(Blade sword) {
 	Sword = sword;
 }
-
+/** 
+* @author Brad
+* @param double
+**/
 public void setGround(double g){
 	ground=g;
 }
